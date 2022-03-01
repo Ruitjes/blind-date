808df8c7fccb439982777dad15d7f1d386abfdd9
Kevin Bevers <kevin.bevers@hotmail.com>
2022-03-01 15:42:47 +0100

    Added a back-end with 2 placeholder services. - Ocelot gateway with swagger docs - feed service ASP.NET CORE API - question service ASP.NET CORE API - All in .net core 5

---
+++ back-end/back-end/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace back_end
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((Host, config) => {
+                config.AddJsonFile("appsettings.json");
+                config.AddJsonFile("ocelot.json");
+
+                // var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                // if (env != null)
+                // {
+                //     config.AddJsonFile($"ocelot.{env}.json", optional: true);
+                // }
+
+            })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:7000/");
+                });
+    }
+}
