0bdf55b5ec6e091ed06b9cb0c3249aca18dc3def
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2022-03-15 12:05:04 +0100

    Admin page
    Create seperate components for questions and users
    
    Display users list in users component
    
    Design admin page

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import {Question} from './models/Question';
+import { Question } from './models/Question';
 import { MatDialog } from '@angular/material/dialog';
 import { PopupDialogComponent } from '../../src/app/popup-dialog/popup-dialog.component';
 import { Answer } from './models/Answer';
-
-const ELEMENT_DATA: Question[] = [
-  {id: 1, question: 'Is this dress pretty?', answers: [new Answer(0, 'no'), new Answer(1, 'yes')]},
-  {id: 2, question: 'What blind restaurants are there?', answers: [new Answer(0, 'Dont know'), new Answer(1, 'yes in amsterdam maybe')]},
-  {id: 3, question: 'Straight or curly hair for a first date?', answers: [new Answer(0, 'Curly!!'), new Answer(1, 'girll curly for sureee')]},
-];
-
-
 
 @Component({
   selector: 'app-root',
@@ -20,21 +12,6 @@ const ELEMENT_DATA: Question[] = [
 export class AppComponent {
   title = 'admin';
 
-  displayedColumns: string[] = ['id', 'question', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-  constructor( public dialog: MatDialog) { }
+  constructor() { }
 
-
-  openAnswers(question: Question): void {
-  console.log(question);
-    const dialogRef = this.dialog.open(PopupDialogComponent, {
-      maxWidth: '50%',
-      data: {Question: question}
-    }); 
-    dialogRef.afterClosed()
-      .subscribe(res => {
-    });
-  
-  }
-  
 }
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatTableModule} from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
+import { UsersComponent } from './users/users.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PopupDialogComponent
+    PopupDialogComponent,
+    UsersComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +27,9 @@ import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
     MatTabsModule,
     MatTableModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
