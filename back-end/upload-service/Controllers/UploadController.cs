eba972f660b802bf21e13452354ff8f5431ccd0d
Stan Rutten <stanrutten242@hotmail.com>
2022-04-01 13:58:36 +0200

    API Endpoint for uploading s3 images

---
+++ back-end/upload-service/Controllers/UploadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using upload_service.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace upload_service.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        private readonly IUploadService _uploadService;
+
+        public UploadController(IUploadService uploadService)
+        {
+            _uploadService = uploadService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Controller is working!");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> UploadFileToS3(IFormFile file)
+        {
+            var result = await _uploadService.UploadFileToS3(file);
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
+
---
+++ back-end/upload-service/Services/UploadService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Mvc;
+using upload_service.Interfaces;
+
+namespace upload_service.Services
+{
+	public class UploadService : IUploadService
+	{
+		public async Task<bool> UploadFileToS3(IFormFile file)
+		{
+            string? yourAccesKey = Environment.GetEnvironmentVariable("ACCESS_KEY");
+            string? yourSecretKey = Environment.GetEnvironmentVariable("SECRET_ACCESS_KEY");
+            string? bucketName = Environment.GetEnvironmentVariable("BUCKET_NAME");
+
+
+            using (var client = new AmazonS3Client(yourAccesKey, yourSecretKey, RegionEndpoint.EUCentral1))
+            {
+                using (var newMemoryStream = new MemoryStream())
+                {
+                    file.CopyTo(newMemoryStream);
+
+                    var uploadRequest = new TransferUtilityUploadRequest
+                    {
+                        InputStream = newMemoryStream,
+                        Key = file.FileName, // filename
+                        BucketName = bucketName // bucket name of S3
+                    };
+
+                    var fileTransferUtility = new TransferUtility(client);
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+            }
+
+            return true;
+		}
+	}
+}
+
