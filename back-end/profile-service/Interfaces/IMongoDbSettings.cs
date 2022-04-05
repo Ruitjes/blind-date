19ca53719f8a39fa8d1864fe60623b772bfa9f1a
Beatrice Forslund <forslund.beatrice@gmail.com>
2022-04-05 13:05:43 +0200

    RUD profiles

---
+++ back-end/profile-service/Interfaces/IMongoDbSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace profile_service.Interfaces
+{
+	public interface IMongoDbSettings
+	{
+		string ConnectionString { get; }
+
+		string DbName { get; }
+
+		string ProfileCollectionName { get; }
+	}
+}
---
+++ back-end/profile-service/Services/ProfileService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using profile_service.Interfaces;
+using profile_service.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace question_service.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IMongoCollection<Profile> _profiles;
+
+        public ProfileService(IMongoDbSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DbName);
+            _profiles = database.GetCollection<Profile>(settings.ProfileCollectionName);
+        }
+
+        public async Task<List<Profile>> GetAllAsync()
+        {
+            return await _profiles.Find(s => true).ToListAsync();
+        }
+
+        public async Task<ObjectId> GetObjectIdByUserIdentifierAsync(string userIdentifier)
+        {
+
+            Profile profile = await _profiles.Find(s => s.UserIdentifier == userIdentifier).FirstOrDefaultAsync();
+            return (ObjectId)profile.Id;
+
+        }
+
+        public async Task<Profile> GetProfileByUserIdentifierAsync(string userIdentifier)
+        {
+
+            Profile profile = await _profiles.Find(s => s.UserIdentifier == userIdentifier).FirstOrDefaultAsync();
+            return profile;
+
+        }
+
+
+
+        public async Task<Profile> CreateAsync(Profile profile)
+        {
+            await _profiles.InsertOneAsync(profile);
+            return profile;
+        }
+
+        public async Task<string> UpdateAsync(ObjectId id, Profile profile)
+        {
+            await _profiles.UpdateOneAsync(x => x.Id == id,
+        Builders<Profile>.Update.Set(p => p.interests, profile.interests));
+            return "Profile updated!";
+        }
+
+        public async Task<string> DeleteAsync(ObjectId id)
+        {
+            await _profiles.DeleteOneAsync(s => s.Id == id);
+            return "Profile deleted!";
+        }
+    }
+}
+
+
