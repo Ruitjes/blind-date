f24b2d29bebdd2ae043fbb96ae141139c83e88d6
Beatrice Forslund <forslund.beatrice@gmail.com>
2022-04-08 09:14:51 +0200

    update program.cs

--- back-end/profile-service/Program.cs
+++ back-end/profile-service/Program.cs
@@ -1,11 +1,24 @@
+using profile_service.Interfaces;
+using profile_service.Models;
+using question_service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Enviroment variables for database connnection.
+string? connectionString = Environment.GetEnvironmentVariable("MongoDbConnectionString");
+string? dbName = Environment.GetEnvironmentVariable("MongoDbName");
+string? profileCollectionName = Environment.GetEnvironmentVariable("MongoDbProfileCollectionName");
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IMongoDbSettings>(new MongoDbSettings(connectionString, dbName, profileCollectionName));
+builder.Services.AddScoped<IProfileService, ProfileService>();
+
+
 
 var app = builder.Build();
 
