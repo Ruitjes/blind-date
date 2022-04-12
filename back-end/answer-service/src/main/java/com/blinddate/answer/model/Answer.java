1ab62c62666a30a042de63102811d966132b0ce9
ofgokbak <vanoisterwijk@icloud.com>
2022-04-12 14:01:53 +0200

    Created an initial draft for question service

---
+++ back-end/answer-service/src/main/java/com/blinddate/answer/model/Answer.java
@@ -0,0 +1,16 @@
+package com.blinddate.answer.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@Builder
+public class Answer {
+    private String id;
+    private String content;
+    private String questionId;
+    private UserProfile userProfile;
+
+}
