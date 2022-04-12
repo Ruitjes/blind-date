5775cc66b9c799db01e276ca98ead2aa2a2e6280
Rick Verhaegh <442124@student.fontys.nl>
2022-04-13 01:33:47 +0200

    chore: change spacing

--- front-end/components/questions-ask/AskQuestionEditBox.tsx
+++ front-end/components/questions-ask/AskQuestionEditBox.tsx
@@ -5,7 +5,7 @@ type Props = {
 const AskQuestionEditBox = (props: Props) => {
 
     return (
-        <div className="flex items-center justify-center border-2 border-gray-200 rounded-lg h-24 overflow-auto drop-shadow-lg">
+        <div className="flex items-center justify-center border-2 border-gray-200 rounded-lg h-32 mt-4 overflow-auto drop-shadow-lg">
             <div contentEditable="true" className="margin-auto text-2xl text-center flex-grow outline-none" onInput={props.onChange}></div>
         </div>
     )
