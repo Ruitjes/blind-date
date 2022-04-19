59fa61ee53273047a7b50cca51e11e700b8dedbb
Stan Rutten <stanrutten242@hotmail.com>
2022-04-19 14:40:54 +0200

    Add Auth0 Login and API call to Angular admin plan

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -12,13 +12,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
 import { UsersComponent } from './users/users.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { AuthButtonComponent } from './auth0/login.component';
+import { UserProfileComponent } from './auth0/user.component';
+import { UserMetadataComponent } from './auth0/metadata.component';
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+
+// Import the injector module and the HTTP client module from Angular
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// Import the HTTP interceptor from the Auth0 Angular SDK
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     PopupDialogComponent,
     UsersComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    AuthButtonComponent,
+    UserProfileComponent,
+    UserMetadataComponent
   ],
   imports: [
     BrowserModule,
@@ -28,9 +42,40 @@ import { QuestionsComponent } from './questions/questions.component';
     MatDialogModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    AuthModule.forRoot({
+      // The domain and clientId were configured in the previous chapter
+      domain: 'blind-date.eu.auth0.com',
+      clientId: 'rlY1Q7b4fqUEk44phOc7JDvXcfEPcT3K',
+    
+      // Request this audience at user authentication time
+      audience: 'https://blind-date.eu.auth0.com/api/v2/',
+    
+      // Request this scope at user authentication time
+      scope: 'read:current_user',
+    
+      // Specify configuration for the interceptor              
+      httpInterceptor: {
+        allowedList: [
+          {
+            // Match any request that starts 'https://blind-date.eu.auth0.com/api/v2/' (note the asterisk)
+            uri: 'https://blind-date.eu.auth0.com/api/v2/*',
+            tokenOptions: {
+              // The attached token should target this audience
+              audience: 'https://blind-date.eu.auth0.com/api/v2/',
+    
+              // The attached token should have these scopes
+              scope: 'read:current_user'
+            }
+          }
+        ]
+      }
+    }),    
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
