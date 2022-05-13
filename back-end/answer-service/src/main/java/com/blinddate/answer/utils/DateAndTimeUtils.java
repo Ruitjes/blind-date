d91b3430decf7866b0eb01eb74a4282ae5a40475
ofgokbak <vanoisterwijk@icloud.com>
2022-05-13 10:15:23 +0200

    Get the date-time with an offset from UTC/Greenwich in the ISO-8601 calendar system.

--- back-end/answer-service/src/main/java/com/blinddate/answer/utils/DateAndTimeUtils.java
+++ back-end/answer-service/src/main/java/com/blinddate/answer/utils/DateAndTimeUtils.java
@@ -1,12 +1,9 @@
 package com.blinddate.answer.utils;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.OffsetDateTime;
 
 public class DateAndTimeUtils {
     public static String getCurrentDateAndTime() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        return dtf.format(now);
+        return OffsetDateTime.now().toString();
     }
 }
