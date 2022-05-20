6e3a9f7d7e0a8f05196f33609d38aa4a3592956f
ofgokbak <vanoisterwijk@icloud.com>
2022-05-20 14:44:10 +0200

    @CrossOrigin allows all origins, all headers, the HTTP methods specified in the @RequestMapping annotation.

--- back-end/answer-service/src/main/java/com/blinddate/answer/conttroller/AnswerController.java
+++ back-end/answer-service/src/main/java/com/blinddate/answer/conttroller/AnswerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/answers")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @AllArgsConstructor
 @Slf4j
 public class AnswerController {
