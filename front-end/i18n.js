cd196f1eb7a92ace124bdfe12cd7f9c8db647c93
Rick Verhaegh <442124@student.fontys.nl>
2022-05-31 10:40:50 +0200

    feat: use language detector for translations

--- front-end/i18n.js
+++ front-end/i18n.js
@@ -1,16 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import XHR from 'i18next-http-backend';
 import * as nl from './lang/nl-NL.json';
 import * as en from './lang/en-US.json';
 
 i18n
+    .use(XHR)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: { en, nl },
-        fallbackLng: ['en', 'nl'],
-        nsSeparator: false,
-        keySeparator: false,
-        compatibilityJSON: 'v3',
+        fallbackLng: 'en',
+        supportedLngs: ['en', 'nl'],
         interpolation: {
             escapeValue: false
         }
