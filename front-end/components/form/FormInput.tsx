a25a2b47e1f1e05c42c892ca3cb08f7359f89266
Kevin Bevers <kevin.bevers@hotmail.com>
2022-06-01 13:59:32 +0200

    Moved skeleton loading into components

--- front-end/components/form/FormInput.tsx
+++ front-end/components/form/FormInput.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { PropsWithChildren } from "react";
 
 type Props = PropsWithChildren<{
+    loading?: boolean,
     type?: React.HTMLInputTypeAttribute | undefined,
     label?: string,
     value?: string | number | readonly string[],
@@ -9,14 +10,18 @@ type Props = PropsWithChildren<{
     onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined,
 }>;
 
-export default function FormInput({label, defaultValue, value, type, onChange}:Props) {
+export default function FormInput({label, defaultValue, value, type, onChange, loading}:Props) {
     return (<>
         <div className="flex flex-wrap -mx-3">
             <div className="w-full px-3">
                 <label className="block text-gray-700 text-xs font-bold mb-2">
                     {label}
                 </label>
+                {loading ? 
+                <div className="appearance-none bg-gray-300 animate-pulse block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-12" />
+                :
                 <input className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type={type} value={value} defaultValue={defaultValue} onChange={onChange} />
+                }
             </div>
         </div>
     </>);
