c72bdf76dcb4d0aaa8a875c9d0e88c1f6815f941
Rick Verhaegh <442124@student.fontys.nl>
2022-06-03 01:39:32 +0200

    fix: resolve console errors

--- front-end/components/BackButton.tsx
+++ front-end/components/BackButton.tsx
@@ -20,8 +20,8 @@ export default function BackButton({navPage}:BackButtonProps) {
                     className="z-50 inline-block p-3 bg-white text-black font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-slate-100 hover:shadow-lg focus:bg-slate-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-200 active:shadow-lg top-5 left-5 fixed"
                     id="btn-back-to-top"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                     </svg>
                 </button>
 
