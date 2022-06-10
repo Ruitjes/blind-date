2c300da654d2c2a6e68046467a2e6257cd0ef092
Beatrice Forslund <forslund.beatrice@gmail.com>
2022-06-10 14:22:20 +0200

    update buttons

--- front-end/components/form/FormWrapper.tsx
+++ front-end/components/form/FormWrapper.tsx
@@ -22,8 +22,8 @@ export default function FormWrapper({ children, onClick,deleteClick, buttonText,
                         {children}  
                     </div>
                     <div className="px-4 py-3 bg-gray-50  space-x-40">
-                    <button aria-label={t("Save")} className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onClick={onClick}>{buttonText}</button>
                     <button aria-label={t("DeleteProfile")} className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" onClick={deleteClick}>{deleteText}</button>
+                    <button aria-label={t("Save")} className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onClick={onClick}>{buttonText}</button>
                     </div>               
                 </div>
             </div>
