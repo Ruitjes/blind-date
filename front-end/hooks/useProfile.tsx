c321566b986727177526d7545137992f793d6744
Rick Verhaegh <442124@student.fontys.nl>
2022-06-10 14:47:26 +0200

    feat: add useProfile hook to fetch profile data

---
+++ front-end/hooks/useProfile.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react"
+import { useTranslation } from "react-i18next";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+type Profile = {
+    id: string;
+    gender: string;
+    language: string;
+}
+
+export const useProfile = () => {
+
+    const { i18n } = useTranslation();
+
+    const [error, setError] = useState<AxiosError>();
+    const [loading, setLoading] = useState<boolean>();
+    const [profile, setProfile] = useState<Profile>();
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get<Profile>("api/profileService/getProfile/")
+            .then((response: AxiosResponse) => setProfile(response.data))
+            .catch((error: AxiosError) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    useEffect(() => {
+        if (profile) {
+            switch (profile.language) 
+            {
+                case "en-US":
+                    i18n.changeLanguage("en");
+                    break;              
+                case "nl-NL":
+                    i18n.changeLanguage("nl");
+                    break;
+            }
+        }
+    }, [profile])
+
+
+    return { error, loading, profile };
+}
